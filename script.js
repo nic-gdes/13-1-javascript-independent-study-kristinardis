47109d67ec118324276a52a3ae77cdddf591e45e
kristinardis <kamcllvenna@nic.edu>
2023-11-18 11:19:04 -0800

    Modal assignment completed with comments
    I had trouble uploading this but somehow the files were OUTSIDE of the assignment.  Hope you get this now!

---
+++ script.js
@@ -0,0 +1,25 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.addEventListener("click", function() {
+  modal.style.display = "block";
+});
+
+// When the user clicks on <span>, close the modal
+span.addEventListener("click", function() {
+  modal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
